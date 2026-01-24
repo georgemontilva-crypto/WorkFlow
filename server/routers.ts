753a8fec70d0cc7bfdd251282b0371e3a093bc1f
georgemontilva-crypto <254040545+georgemontilva-crypto@users.noreply.github.com>
2026-01-24 17:00:59 -0500

    fix: Agregar importación faltante de getRedisClient
    - Importar getRedisClient desde ./config/redis
    - Soluciona error: ReferenceError: getRedisClient is not defined
    - Afectaba endpoints requestPasswordReset y verifyPasswordReset2FA

--- server/routers.ts
+++ server/routers.ts
@@ -4,6 +4,7 @@ import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, protectedProcedure, superAdminProcedure, router } from "./_core/trpc";
 import { z } from "zod";
 import * as db from "./db";
+import { getRedisClient } from "./config/redis";
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
